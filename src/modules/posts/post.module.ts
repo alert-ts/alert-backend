62b60fe8c7fb897ecf82a50cd7ce3b94ecdecb51
Raisess <raisechif@gmail.com>
2021-05-16 15:20:07 -0300

    apply auth middleware to posts module

--- src/modules/posts/post.module.ts
+++ src/modules/posts/post.module.ts
@@ -1,10 +1,16 @@
-import { Module } from "@nestjs/common";
+import { Module, NestModule, MiddlewareConsumer } from "@nestjs/common";
 
 import { PostController } from "./post.controller";
 import { PostService } from "./post.service";
+
+import { AuthMiddleware } from "../../common/middlewares/auth.middleware";
 
 @Module({
   controllers: [PostController],
   providers: [PostService],
 })
-export class PostModule {}
+export class PostModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(PostController);
+  }
+}
