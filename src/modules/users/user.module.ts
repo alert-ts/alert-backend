d63e16e1d7909b0043b9be603aaab61ddb2f2627
Raisess <raisechif@gmail.com>
2021-05-16 14:52:46 -0300

    added auth middleware to users actions

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,10 +1,27 @@
-import { Module } from "@nestjs/common";
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from "@nestjs/common";
 
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
+
+import { AuthMiddleware } from "../../common/middlewares/auth.middleware";
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({
+        path: "users",
+        method: RequestMethod.POST,
+      })
+      .forRoutes(UserController);
+  }
+}
