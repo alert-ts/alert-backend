cc4ffaa2d783885d322e89c4c19943a395132204
Raisess <raisechif@gmail.com>
2021-05-16 14:33:04 -0300

    updated auth middleware

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -11,20 +11,22 @@ import JWT from "../../shared/JWT";
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   public use(req: Request, res: Response, next: NextFunction): void {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.split(" ").includes("Bearer")
-    ) {
-      const token: string = req.headers.authorization.split(" ")[1];
+    try {
+      if (
+        req.headers.authorization &&
+        req.headers.authorization.split(" ").includes("Bearer")
+      ) {
+        const token: string = req.headers.authorization.split(" ")[1];
 
-      if (new JWT().verify(token)) {
+        new JWT().verify(token);
+
         return next();
       }
+    } catch (err) {
+      throw new HttpException(
+        "Invalid authorization token",
+        HttpStatus.UNAUTHORIZED,
+      );
     }
-
-    throw new HttpException(
-      "Invalid authorization token",
-      HttpStatus.UNAUTHORIZED,
-    );
   }
 }
